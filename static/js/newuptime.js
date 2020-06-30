c5024496eea954d682de354595c52ba64a745a55
赤琦 <redblue9771@gmail.com>
2020-06-30 19:57:39 +0800

    feat: 更新 API 获取

--- static/js/newuptime.js
+++ static/js/newuptime.js
@@ -1,110 +1,146 @@
 getMonitorsByApi();
 
 function getMonitorsByApi() {
-    // fetch("https://api.fjut.eu.org/getUptime.php?user=wKLkwH4ZG&type=0").then(res => {
-    //     if (res.ok) {
-    //         return res.json();
-    //     } else {
-    //         fetch("https://fjutwzk.heliohost.org/api/getUptime.php?user=wKLkwH4ZG&type=0").then(res1 => {
-    //             if (res1.ok) {
-    //                 return res.json();
-    //             } else {
-    //                 alert("Data acquisition failed, please refresh and try again!");
-    //                 $fresh.textContent = "-";
-    //             }
-    //         }).then(res1 => {
-    //             if (res1.stat === "ok") {
-    //                 renderHTML(res1);
-    //             }
-    //         });
-    //     }
-    // }).then(res => {
-    //     if (res.stat === "ok") {
-    //         renderHTML(res);
-    //     }
-    // });
-    fetch("https://api.fjut.eu.org/getUptime.php?user=wKLkwH4ZG&type=3").then(res => {
-        if (res.ok) {
-            return res.json();
-        } else {
-            alert("Data acquisition failed, please refresh and try again!");
-            $fresh.textContent = "-";
-        }
-    }).then(res => {
+  fetch("https://api.uptimerobot.com/v2/getMonitors", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      api_key: "ur569559-000946232f7a2cb0ea09c63f",
+      timezone: "1",
+      logs: "1",
+      log_types: "1",
+      custom_uptime_ratios: "1-7-30-45",
+    }),
+  })
+    .then((res) => {
+      if (res.ok && res.status === 200) {
+        return res.json();
+      } else {
+        $fresh.textContent = "-";
+        return {};
+      }
+    })
+    .then((res) => {
+      // console.log(res);
+      if (res.stat === "ok") {
+        console.log(res);
         renderHTML(res);
-    });
+      }
+    })
+    .catch(console.log);
 }
 
 function renderHTML(res = {}) {
-    let statusFlag = {
-            "success": "green",
-            "Down": "yellow",
-            "Seems Down": "orange",
-            "Paused": "black",
-            "Not Checked Yet": "black"
-        },
-        lastName = ["l1", "l7", "l30"],
+  const statusClass = (idx) =>
+      (idx === 0 && "black") ||
+      (idx === 1 && "black") ||
+      (idx === 2 && "green") ||
+      (idx === 8 && "orange") ||
+      (idx === 9 && "yellow") ||
+      "",
+    statusStr = (idx) =>
+      (idx === 0 && "Paused") ||
+      (idx === 1 && "Not checked yet") ||
+      (idx === 2 && "Up") ||
+      (idx === 8 && "Seems down") ||
+      (idx === 9 && "Down") ||
+      "",
+    lastName = ["l1", "l7", "l30"],
+    ctx = document.getElementById("myChart"),
+    $table = document.getElementById("domainList"),
+    $overallUptime = document.getElementById("overallUptime"),
+    $overallUptimeValue = $overallUptime.getElementsByClassName("value"),
+    $overallUptimeDown = $overallUptime.getElementsByClassName("down"),
+    $downTime = document.getElementById("downtime"),
+    allRatios = [0, 0, 0, 0],
+    statusCount = [0, 0, 0, 0, 0];
+  let html = "";
+  console.log(statusClass(1));
+  for (const item of res.monitors) {
+    switch (item.status) {
+      case 0:
+        statusCount[0]++;
+        break;
+      case 1:
+        statusCount[1]++;
+        break;
+      case 2:
+        statusCount[2]++;
+        break;
+      case 8:
+        statusCount[3]++;
+        break;
+      case 9:
+        statusCount[4]++;
+        break;
 
-        ctx = document.getElementById("myChart"),
-        $table = document.getElementById("domainList"),
-        $overallUptime = document.getElementById("overallUptime"),
-        $overallUptimeValue = $overallUptime.getElementsByClassName("value"),
-        $overallUptimeDown = $overallUptime.getElementsByClassName("down"),
-        $downTime = document.getElementById("downtime"),
-        html = '';
-    for (const item of res.psp.monitors) {
-        html += '<tr><td><span class="ui ' + statusFlag[item.statusClass] + ' empty circular label"></span> ' +
-            item.statusClass + '</td><td>' + item.weeklyRatio.ratio + '%</td><td class="selectable"><a href="https://' + item.name + '" target="_blank">' + item.name + '</a></td><tr>';
-    }
-    $table.innerHTML = html;
-    var data = {
-        datasets: [{
-            data: [res.statistics.counts.up, res.statistics.counts.down, res.statistics.counts.pasued],
-            backgroundColor: [
-                '#448aff',
-                '#f44336',
-                'yellow'
-            ]
-        }],
-        // These labels appear in the legend and in the tooltips when hovering different arcs
-        labels: [
-            'Up',
-            'Down',
-            'Paused'
-        ]
-    };
-    new Chart(ctx, {
-        type: 'bar',
-        data: data,
-        options: {
-            legend: {
-                display: false,
-            }
-        }
-    });
-
-    {
-        let i = 0;
-        for (const key in res.statistics.uptime) {
-            if (res.statistics.uptime.hasOwnProperty(key)) {
-                const element = res.statistics.uptime[key];
-                $overallUptimeValue[i].textContent = element.ratio + '%';
-                $overallUptimeDown[i].textContent = element.downtime;
-                i++;
-            }
-        }
+      default:
+        break;
     }
 
+    const ratios = item.custom_uptime_ratio.split("-");
+    allRatios[0] += Number(ratios[0]);
+    allRatios[1] += Number(ratios[1]);
+    allRatios[2] += Number(ratios[2]);
+    allRatios[3] += Number(ratios[3]);
+    html +=
+      '<tr><td><span class="ui ' +
+      statusClass(item.status) +
+      ' empty circular label"></span> ' +
+      statusStr(item.status) +
+      '</td><td class="selectable"><a href="' +
+      item.url +
+      '" target="_blank">' +
+      item.friendly_name +
+      "</a></td><td>" +
+      ratios[0] +
+      "%</td><td>" +
+      ratios[1] +
+      "%</td><td>" +
+      ratios[2] +
+      "%</td><td>" +
+      ratios[3] +
+      "%</td><tr>";
+  }
+  $table.innerHTML = html;
+  const data = {
+    datasets: [
+      {
+        data: statusCount,
+        backgroundColor: ["black", "black", "green", "orange", "yellow"],
+      },
+    ],
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: ["Paused", "Not checked yet", "Up", "Seems down", "Down"],
+  };
+  new Chart(ctx, {
+    type: "bar",
+    data: data,
+    options: {
+      legend: {
+        display: false,
+      },
+    },
+  });
 
-    $downTime.textContent = res.statistics.latest_downtime;
-    setTimeout('getMonitorsByApi()', 61000);
-    let $fresh = document.getElementById("fresh"),
-        i = 60,
-        label = setInterval(() => {
-            if (i == 0) {
-                clearInterval(label);
-            }
-            $fresh.textContent = i;
-            i--;
-        }, 1000);
+  allRatios.forEach((ratio, index) => {
+    console.log(ratio, res.monitors.length);
+    $overallUptimeValue[index].textContent = `${(
+      ratio / res.monitors.length
+    ).toFixed(3)}%`;
+  });
+
+  // $downTime.textContent = res.statistics.latest_downtime;
+  // setTimeout("getMonitorsByApi()", 61000);
+  // let $fresh = document.getElementById("fresh"),
+  //   i = 60,
+  //   label = setInterval(() => {
+  //     if (i == 0) {
+  //       clearInterval(label);
+  //     }
+  //     $fresh.textContent = i;
+  //     i--;
+  //   }, 1000);
 }
--- static/js/uptime.js
+++ static/js/uptime.js
@@ -1,85 +1,102 @@
 getMonitorsByApi("https://api.uptimerobot.com/v2/getMonitors", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body: "api_key=u569559-c0e7f28f93357de5cf044f57&format=json&logs=1&all_time_uptime_ratio=1&custom_uptime_ratios=1-7-30"
+  method: "POST",
+  body: {
+    api_key: "ur569559-000946232f7a2cb0ea09c63f",
+    timezone: "1",
+    logs: "1",
+    log_types: "1",
+    custom_uptime_ratios: "7",
+  },
+  // headers: {
+  //     "Content-Type": "application/x-www-form-urlencoded"
+  // },
+  // body: "api_key=u569559-c0e7f28f93357de5cf044f57&format=json&logs=1&all_time_uptime_ratio=1&custom_uptime_ratios=1-7-30"
 });
 // By uptimerobot
-function getMonitorsByApi(url = '', setGet = {}) {
-    let domainStatus = {
-            "0": "Paused",
-            "1": "Not Checked Yet",
-            "2": "Up",
-            "8": "Seems Down",
-            "9": "Down",
-        },
-        statusFlag = {
-            "Up": "green",
-            "Down": "yellow",
-            "Seems Down": "orange",
-            "Paused": "black",
-            "Not Checked Yet": "black"
-        },
-        statusCount = {
-            '0': 0,
-            '1': 0,
-            '2': 0,
-            '8': 0,
-            '9': 0,
-        };
+function getMonitorsByApi(url = "", setGet = {}) {
+  let domainStatus = {
+      "0": "Paused",
+      "1": "Not Checked Yet",
+      "2": "Up",
+      "8": "Seems Down",
+      "9": "Down",
+    },
+    statusFlag = {
+      Paused: "black",
+      "Not Checked Yet": "black",
 
-    fetch(url, setGet).then(res => res.json()).then(res => {
-        if (res.stat === 'ok') {
-            var res = res.monitors,
-                len = res.length,
-                ctx = document.getElementById("myChart"),
-                $table = document.getElementById("tableBody"),
-                $overallUptime = document.getElementsByClassName("upVal"),
-                html = '',
-                uptime = [],
-                timeTotal = [0, 0, 0];
+      Up: "green",
 
-            for (const item of res) {
-                uptime = item.custom_uptime_ratio.split('-');
-                html += '<tr><td><span class="ui ' + statusFlag[domainStatus[item.status]] + ' empty circular label"></span> ' +
-                    domainStatus[item.status] + '</td><td>' + uptime[0] + '%</td><td>' + item.friendly_name + '<a href="' + item.url + '" target="_blank"> ✈</i></a></td><tr>';
-                statusCount[item.status]++;
-                timeTotal[0] += Number(uptime[0]);
-                timeTotal[1] += Number(uptime[1]);
-                timeTotal[2] += Number(uptime[2]);
-            }
-            $table.innerHTML = html;
-            var data = {
-                datasets: [{
-                    data: [statusCount[2], statusCount[9], statusCount[0]],
-                    backgroundColor: [
-                        '#448aff',
-                        '#f44336',
-                        'yellow'
-                    ]
-                }],
-                // These labels appear in the legend and in the tooltips when hovering different arcs
-                labels: [
-                    'Up',
-                    'Down',
-                    'Paused'
-                ]
-            };
-            new Chart(ctx, {
-                type: 'bar',
-                data: data,
-                options: {
-                    legend: {
-                        display: false,
-                    }
-                }
-            });
-            let i = 0;
-            for (const item of $overallUptime) {
-                item.textContent = Math.round(timeTotal[i] / len * 1000) / 1000 + '%';
-                i++;
-            }
+      "Seems Down": "orange",
+      Down: "yellow",
+    },
+    statusCount = {
+      "0": 0,
+      "1": 0,
+      "2": 0,
+      "8": 0,
+      "9": 0,
+    };
+
+  fetch(url, setGet)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      if (res.stat === "ok") {
+        var res = res.monitors,
+          len = res.length,
+          ctx = document.getElementById("myChart"),
+          $table = document.getElementById("tableBody"),
+          $overallUptime = document.getElementsByClassName("upVal"),
+          html = "",
+          uptime = [],
+          timeTotal = [0, 0, 0];
+
+        for (const item of res) {
+          uptime = item.custom_uptime_ratio.split("-");
+          html +=
+            '<tr><td><span class="ui ' +
+            statusFlag[domainStatus[item.status]] +
+            ' empty circular label"></span> ' +
+            domainStatus[item.status] +
+            "</td><td>" +
+            uptime[0] +
+            "%</td><td>" +
+            item.friendly_name +
+            '<a href="' +
+            item.url +
+            '" target="_blank"> ✈</i></a></td><tr>';
+          statusCount[item.status]++;
+          timeTotal[0] += Number(uptime[0]);
+          timeTotal[1] += Number(uptime[1]);
+          timeTotal[2] += Number(uptime[2]);
         }
+        $table.innerHTML = html;
+        var data = {
+          datasets: [
+            {
+              data: [statusCount[2], statusCount[9], statusCount[0]],
+              backgroundColor: ["#448aff", "#f44336", "yellow"],
+            },
+          ],
+          // These labels appear in the legend and in the tooltips when hovering different arcs
+          labels: ["Up", "Down", "Paused"],
+        };
+        new Chart(ctx, {
+          type: "bar",
+          data: data,
+          options: {
+            legend: {
+              display: false,
+            },
+          },
+        });
+        let i = 0;
+        for (const item of $overallUptime) {
+          item.textContent =
+            Math.round((timeTotal[i] / len) * 1000) / 1000 + "%";
+          i++;
+        }
+      }
     });
 }
